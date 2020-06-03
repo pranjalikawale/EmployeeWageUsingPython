e1ddf10156f898eb1cca439a65098d9066d1eb53
pranjalikawale <pranjalikawale1578@gmail.com>
2020-06-03 09:59:35 +0530

    ADDED:Functionality to calculate employee wage for parttime

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -6,16 +6,20 @@ print("----Welcome To Employee Wage Problem----")
 
 #declare constant
 IS_FULL_TIME=1
+IS_PART_TIME=2
 EMP_RATE_HRS=20
 
-#declare variable for attendence, employeeWage and employeeHrs
+#declare variable for attendence, employeeWage
 attendence=math.floor(random.random()*10)%2
 empWage=0
-empHrs=0
 
 #ckeck for employee is present or not
 if attendence == IS_FULL_TIME:
     empHrs=8
+elif attendence == IS_PART_TIME:
+    empHrs=4
+else:
+    empHrs=0
 
 #calculate employee wage    
 empWage=empHrs*EMP_RATE_HRS
