4800be066ce7143c6fab72e3f5f45f6eacdddf73
pranjalikawale <pranjalikawale1578@gmail.com>
2020-06-03 10:34:18 +0530

    ADDED:Functionality to calculate employee wage using switcher

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -10,19 +10,33 @@ IS_PART_TIME=2
 EMP_RATE_HRS=20
 
 #declare variable for attendence, employeeWage
-attendence=math.floor(random.random()*10)%2
+attendence=math.floor(random.random()*10)%3
 empWage=0
 
-#ckeck for employee is present or not
-if attendence == IS_FULL_TIME:
-    empHrs=8
-elif attendence == IS_PART_TIME:
-    empHrs=4
-else:
-    empHrs=0
+#return fulltime employee working hrs
+def isFullTime():
+    return 8
+
+#return parttime employee working hrs
+def isPartTime():
+    return 4    
+
+#return zero employee working hrs
+def isAbsent():
+    return 0
+
+#ckeck for employee working hrs
+switcher={
+    0:isAbsent,
+    1:isFullTime,
+    2:isPartTime,
+}
+
+# Get the function from switcher dictionary
+func=switcher.get(attendence)
 
 #calculate employee wage    
-empWage=empHrs*EMP_RATE_HRS
+empWage=func()*EMP_RATE_HRS
 
 #print employee wage
 print("Employ Wage: {0}".format(empWage))
