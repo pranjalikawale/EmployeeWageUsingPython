99630179120f68f4ea14f12aae9c6fc3273f3599
pranjalikawale <pranjalikawale1578@gmail.com>
2020-06-03 09:50:13 +0530

    ADDED:Functionality to calculate employee wage for fulltime

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -1,12 +1,24 @@
 import math
 import random
 
+#welcome msg
 print("----Welcome To Employee Wage Problem----")
 
+#declare constant
 IS_FULL_TIME=1
-attendence=math.floor(random.random()*10)%2
+EMP_RATE_HRS=20
 
+#declare variable for attendence, employeeWage and employeeHrs
+attendence=math.floor(random.random()*10)%2
+empWage=0
+empHrs=0
+
+#ckeck for employee is present or not
 if attendence == IS_FULL_TIME:
-    print("Employee is Present")
-else:
-    print("Employee is Absent")
+    empHrs=8
+
+#calculate employee wage    
+empWage=empHrs*EMP_RATE_HRS
+
+#print employee wage
+print("Employ Wage: {0}".format(empWage))
