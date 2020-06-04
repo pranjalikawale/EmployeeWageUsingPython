15c4978e09309bb7d3bf122cd4ed82fe848ff8ac
pranjalikawale <pranjalikawale1578@gmail.com>
2020-06-04 12:03:15 +0530

    ADDED:Used To Comput employee wage for multiple company

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -11,7 +11,8 @@ class EmployWageComputation:
     IS_PART_TIME=2
 
     #initialize class variable
-    def __init__(self,emp_rate,number_of_days,number_of_hrs):
+    def __init__(self,company_name,emp_rate,number_of_days,number_of_hrs):
+        self.company=company_name
         self.emp_rate=emp_rate
         self.number_of_days=number_of_days
         self.number_of_hrs=number_of_hrs    
@@ -52,19 +53,21 @@ class EmployWageComputation:
     #compute employ wage
     def compute_employee_wage(self):
         total_working_day=0
-        total_working_hrs=0 
+        total_Working_hrs=0 
         hours=0
 
         #calculate mothly employee wage
-        while total_working_hrs<self.number_of_hrs  and total_working_day<self.number_of_days:
+        while total_Working_hrs<self.number_of_hrs  and total_working_day<self.number_of_days:
             hours=self.get_hour()
-            total_working_hrs+=hours
+            total_Working_hrs+=hours
             total_working_day+=1
-            print(("Employ Wage for day {0} : {1}").format(total_working_day,self.get_daily_wage(hours)))
+            print(("{0} Employ Wage for day {1} : {2}").format(self.company,total_working_day,self.get_daily_wage(hours)))
 
         #print employee wage    
-        print("Employ Wage for month : {0} ".format(self.get_daily_wage(total_working_hrs)))
+        print("{0} Employ Wage for month : {1} \n".format(self.company,self.get_daily_wage(total_Working_hrs)))
 
 #main
-employee=EmployWageComputation(20,20,100)
-employee.compute_employee_wage()
+tcs=EmployWageComputation("TCS",20,20,100)
+jio=EmployWageComputation("JIO",20,20,100)
+tcs.compute_employee_wage()
+jio.compute_employee_wage()
